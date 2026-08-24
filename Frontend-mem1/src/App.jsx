289047d2567d6ba142a3fb3c88a42f9885a03829
Nihar <gundanihar@gmail.com>
2026-08-24 14:27:31 +0530

    Fixed import path for Signup component

--- Frontend-mem1/src/App.jsx
+++ Frontend-mem1/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import Signup from "./pages/SignUp";
 import Home from "./pages/Home";
 import GradeDashboard from "./pages/GradeDashboard";
 import SubjectSelection from "./pages/SubjectSelection";
